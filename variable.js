530b50510d4f9d833eb1858c09970bd9d4b739be
no1rain <no1rain@gmail.com>
2022-08-03 21:27:49 +0900

    1일차 Variable공부

---
+++ variable.js
@@ -0,0 +1,87 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript
+'use strict';
+
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+{
+    let name = 'john';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+    console.log(globalName);
+}
+//console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+    age = 7;
+    var age;
+}
+console.log(age);
+
+
+// 3. constant
+// favor immutable data type always for a few reason:
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+
+// 4. Variable types
+// primitive single item: number, string, boolean, null, undefined,symbol
+// object, box container
+// function, first-class function
+const count = 17;   // integer
+const size = 17.1;  // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+const helloBob = `hi ${brendan}!`;  // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value:' + helloBob + ' type: ' + typeof helloBob);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1;
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2);
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
