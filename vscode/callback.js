a80bc152ee9b1218ecd972dd6ff9c2183b5eb57d
no1rain <no1rain@gmail.com>
2022-08-08 20:44:47 +0900

    6일차 callback 공부

---
+++ vscode/callback.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// JavaScript is synchronous.
+// Execute the code block by orger after hoisting.
+// hoisting: var, function declaration
+console.log('1');
+
+setTimeout(function() {
+    console.log('2');
+}, 1000);
+
+setTimeout(() => console.log('3'), 1000);
+
+console.log('4');
+
+// Synchronous callback
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'), 2000);
+
+// Callback Hell example
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(() => {
+            if(
+                (id === 'ellie' && password === 'dream') ||
+                (id === 'coder' && password === 'academy')
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRole(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'ellie') {
+                onSuccess({ name: 'ellie', role: 'admin' });
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id,
+    password,
+    user => {
+        userStorage.getRole(
+            user,
+            userWithRole => {
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    },
+    error => {
+        console.log(error);
+    }
+);
