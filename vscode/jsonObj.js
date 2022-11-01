0d0f0a31d3a94c82380f8325476348e989278299
no1rain <no1rain@gmail.com>
2022-11-01 23:00:28 +0900

    JSON 정규식 match

--- vscode/jsonObj.js
+++ vscode/jsonObj.js
@@ -3,13 +3,23 @@
 
 'use strict';
 
-let data = { "PLT_CODE": [ {item_cd: "A1234567", item_name: "Moon IT'S fire!!!", qty: 20}, {item_cd: "A7654321", item_name: "Sole IT\\'S Volt", qty: 370} ]};
+let data = { "PLT_CODE": [  {item_cd: "A1234567", item_name: "Moon IT'S fire!!!", qty: 20}, 
+                            {item_cd: "A7654321", item_name: "Sole IT\'S Volt", qty: 371},
+                            {item_cd: "A7654321", item_name: "Sole IT\\'S Volt", qty: 382},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\'S Volt", qty: 503},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\'S Volt", qty: 594},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\'S Volt", qty: 725},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\\'S Volt", qty: 776},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\\\'S Volt", qty: 837},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\\\\'S Volt", qty: 848},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\\\\\'S Volt", qty: 899},
+                            {item_cd: "A7654321", item_name: "Sole IT\\\\\\\\\\'S Volt", qty: 990}, ]};
 
 let jsondata = JSON.stringify(data);
 
 console.log("jsondata_org=====" + jsondata);
 
-let reg = /\\{0,8}\'/g;
+const reg = /\\{0,8}\'/g;
 
 if(reg.test(jsondata)) {
     console.log("match");
